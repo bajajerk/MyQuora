77fcdbab133583fcaf8d158483e5f4edb60b90ae
Mayank <bajaj3743@gmail.com>
2017-07-17 05:38:00 +0530

    upvote and vote fully fixed

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,21 @@
 class Answer < ActiveRecord::Base
   belongs_to :User
   belongs_to :question
+
+
+
+    def upVoted_by user_id
+  	UpvoteAnswer.where(answer_id: id, User_id: user_id).length > 0
+  end
+
+
+  def upVoted_string user_id
+  	if upVoted_by user_id
+  		return "Cancel Upvote"
+  	else
+  		return "Upvote"
+  	end
+
+  end
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,20 @@
 class Question < ActiveRecord::Base
   belongs_to :User
+
+
+
+    def upVoted_by user_id
+  	UpvoteQuestion.where(question_id: id, User_id: user_id).length > 0
+  end
+
+
+  def upVoted_string user_id
+  	if upVoted_by user_id
+  		return "Cancel Upvote"
+  	else
+  		return "Upvote"
+  	end
+
+  end
+
 end
